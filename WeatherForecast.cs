dfba2aa64f4e7345bab04550782f6bff60b2bc9f
04720916M <aida.kastner@betterconsultants.es>
2024-09-30 15:08:20 +0200

    prueba

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -8,6 +8,6 @@ namespace AppProfesores
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summary1 { get; set; }
     }
 }
